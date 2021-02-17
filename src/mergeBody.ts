da46da5eae45ba3130d5a930ad90012cb743391e
hrdtbs <harada.tsubasa.0422@gmail.com>
2021-02-17 09:31:32 +0900

    refactor(mergeBody): set defalt values

--- src/mergeBody.ts
+++ src/mergeBody.ts
@@ -1,6 +1,6 @@
 import { CONTENT_REGEXP } from "./constants";
 
-export const mergeBody = (current: string, next: string) => {
+export const mergeBody = (current: string = "", next: string = "") => {
   return current
     ? CONTENT_REGEXP.test(current)
       ? current.replace(CONTENT_REGEXP, next)
