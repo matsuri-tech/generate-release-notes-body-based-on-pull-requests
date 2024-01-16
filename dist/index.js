918478d15fcabf5cfc244eed0a0e568b978adf74
hrdtbs <harada.tsubasa.0422@gmail.com>
2024-01-16 16:44:32 +0900

    chore: build

--- dist/index.js
+++ dist/index.js
@@ -29013,6 +29013,7 @@ function run() {
                 return;
             }
             const commits = yield octokit.rest.pulls.listCommits(Object.assign(Object.assign({}, context.repo), { pull_number: context.payload.pull_request.number }));
+            core.debug("commits.data: " + JSON.stringify(commits.data, null, 2));
             const pulls = yield Promise.all(commits.data
                 .filter((commit) => {
                 return commit.commit.message.startsWith("Merge pull request");
